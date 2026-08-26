9e3a844e56f31b38ff2bb8f87da3199dc8db6f7d
Gregory S. Tucker <gregory.tucker@ess.eu>
2026-08-26 16:40:53 +0200

    [Ref] BIFROST instrument

--- src/niess/bifrost/bifrost.py
+++ src/niess/bifrost/bifrost.py
@@ -1,19 +1,34 @@
-from dataclasses import dataclass
+
+from ..instrument import Instrument, Mount, InstrumentParameter
+from .primary import Primary
+from .tank import Tank
+from ..utilities import calibration
+from ..components.component import Component
+from scipp import vector
+from scipp.spatial import rotations_from_rotvecs
+
+@calibration
+def instrument(params: dict):
+    sample = params.pop('sample', None)
+    primary = Primary.from_calibration(**params)
+    tank = Tank.from_calibration(**params)
+    origin='sample_origin'
+    a3 = InstrumentParameter.parse('a3/"deg" = 0')
+    a4 = InstrumentParameter.parse('a4/"deg" = 0')
+
+    if sample is None:
+        sample = Component(
+            name='sample',
+            position=vector((0, 0, 0), unit='m'),
+            orientation=rotations_from_rotvecs(vector([0, 0, 0], unit='deg'))
+        )
+
+    return Instrument(
+        name='bifrost', origin=origin, parts=(
+            Mount(name='primary', content=primary),
+            Mount(name='sample', rotation=(0, a3, 0), relative_to=origin, content=sample),
+            Mount(name='tank', rotation=(0, a4, 0), relative_to=origin, content=tank),
+        ), parameters=(a3, a4))
 
 
-@dataclass
-class BIFROST:
-    from ..components import IndirectSecondary
-    # primary: BandwidthPrimary   # A minimal form of the primary spectrometer necessary to transform events
-    secondary: IndirectSecondary  # A minimal form of the secondary spectrometer necessary to transform events
-
-    @staticmethod
-    def from_calibration(**params):
-        from scipp import vector
-        from .tank import Tank
-        # primary = ...
-        params['sample'] = params.get('sample', vector([0, 0, 0.], unit='m'))
-        tank = Tank.from_calibration(**params)
-
-        secondary = tank.to_secondary(sample=params['sample'])
-        return BIFROST(secondary)
+BIFROST = instrument()
